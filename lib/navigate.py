165afe054dd7d08dd7cee6618d0dc8bf0d7e5b6c
Alex Kalicki <alexander.kalicki@gmail.com>
2014-01-10 15:03:35 -0500

    added basic local OS navigation: pwd, ls, cd

---
+++ lib/navigate.py
@@ -0,0 +1,74 @@
+"""
+    stratus // navigate.py
+    Author: Alex Kalicki (https://github.com/akalicki)
+"""
+
+import os
+
+def list_commands():
+    """Returns a list of all navigation commands"""
+    return {'pwd', 'spwd', 'ls', 'sls', 'cd', 'scd', 'smkdir', 'srmdir'}
+
+def print_help():
+    """Prints the navigation help prompts"""
+    print "\n# Navigation:"
+    print "  pwd  - get path of local directory"
+    print "  spwd - get path of stratus directory"
+    print "  ls   - list files in local directory"
+    print "  sls  - list files in stratus directory"
+
+def process_command(args):
+    cmd = args[0]
+    if cmd == 'pwd' and len(args) == 1:
+        pwd()
+    elif cmd == 'spwd' and len(args) == 1:
+        spwd()
+    elif cmd == 'ls' and len(args) == 1:
+        ls()
+    elif cmd == 'sls' and len(args) == 1:
+        sls()
+    elif cmd == 'cd' and len(args) == 2:
+        cd(args[1])
+    elif cmd == 'scd' and len(args) == 2:
+        scd(args[1])
+    elif cmd == 'smkdir' and len(args) == 2:
+        smkdir(args[1])
+    elif cmd == 'srmdir' and len(args) == 2:
+        srmdir(args[1])
+    else:
+        print "Error: wrong number of arguments, please try again."
+
+def pwd():
+    """Prints the absolute path of the current local directory"""
+    print os.getcwd()
+
+def spwd():
+    """Prints the absolute path of the current stratus directory"""
+
+def ls():
+    """Lists all files and folders in the current local directory"""
+    full = os.listdir(os.curdir)
+    for f in full:
+        if not f.startswith('.'):
+            if os.path.isfile(f): print f
+            elif os.path.isdir(f): print f + '/'
+
+def sls():
+    """Lists all files and folders in the current stratus directory"""
+
+def cd(dir_name):
+    """Move into the given local directory"""
+    path = os.path.expanduser(dir_name)
+    if os.path.isdir(path):
+        os.chdir(path)
+    else:
+        print "Error: '" + cd + "' is not a valid directory."
+
+def scd(dir):
+    """Move into the given stratus directory"""
+
+def smkdir(dir):
+    """Create a stratus directory from the current path"""
+
+def srmdir(dir):
+    """Delete a stratus directory at the current path"""
--- stratus.py
+++ stratus.py
@@ -1,23 +1,38 @@
 """
-    stratus.py
+    stratus - v0.1.0
+    Author: Alex Kalicki (https://github.com/akalicki)
+
+    Free unlimited cloud storage!
+
+    All work contained in this file is released under the MIT License.
+    Please contact the copyright holder with further questions.
 """
 
-import readline, os
+import readline
 from lib import *
 
-print "\nstratus.py - Unlimited cloud storage!" 
+def print_help():
+    print "\nstratus - v0.1.0"
+    print "---------------------"
+    print "help   - list all commands"
+    print "logout - exit the program (aliases: exit, quit)"
+    navigate.print_help()
+    print ""
+
+print "\nstratus.py - Free unlimited cloud storage!" 
 print "Enter a command, or type 'help' to get started.\n"
 
 running = True
 while running:
-    cmd = raw_input('> ')
-    if cmd == 'help':
-        print "\nstratus.py - v0.1.0"
-        print "---------------------"
-        print "help   - list all commands"
-        print "logout - exit the program (aliases: exit, quit)\n"
-    elif cmd == 'exit' or cmd == 'logout' or cmd == 'quit':
+    user_input = raw_input('> ')
+    args = user_input.split(' ')
+    cmd = args[0]
+    if cmd == 'help' and len(args) == 1:
+        print_help()
+    elif cmd in {'exit', 'logout', 'quit'} and len(args) == 1:
         running = False
+    elif cmd in navigate.list_commands():
+        navigate.process_command(args)
     else:
-        print "\nNot a valid command."
-        print "Enter a valid command or type 'help' to get started.\n"
+        print "Error: '" + user_input + "' is not defined."
+        print "Enter a valid command or type 'help' to get started."
