54dd02348f1ce48d634fa38f53d51e67e4c5b8cb
Alex Kalicki <alexander.kalicki@gmail.com>
2014-01-11 21:52:39 -0500

    Finished basic implementation of navigation commands.

--- lib/db.py
+++ lib/db.py
@@ -3,31 +3,45 @@
     Author: Alex Kalicki (https://github.com/akalicki)
 """
 
+from navigate import split_path
 from pymongo import MongoClient
 
 client = MongoClient()
 db = client.stratus
 
-def directoryExists(parent, name):
-    """returns whether the given d irectory already exists"""
+def directory_exists(parent, name):
+    """Returns whether the given directory already exists"""
     return db.dirs.find({'parent': parent, 'name': name}).count() > 0
 
-def createDirectory(parent, name):
+def directory_empty(parent, name):
+    """Returns whether the given directory is empty"""
+    if parent == '':
+        abs_path = name
+    else:
+        abs_path = parent + '/' + name
+    return db.dirs.find({'parent': abs_path}).count() == 0
+
+def create_directory(parent, name):
     """creates stratus directory with given name at the current path"""
-    if directoryExists(parent, name):
+    up_parent, up_name = split_path(parent)
+    if up_parent is not None and not directory_exists(up_parent, up_name):
+        print "Error: '" + up_name + "' is not a valid directory."""
+    elif directory_exists(parent, name):
         print "Error: '" + name + "' already exists."
     else:
         new_dir = {'parent': parent, 'name': name}
         db.dirs.insert(new_dir)
 
-def removeDirectory(parent, name):
+def remove_directory(parent, name):
     """deletes stratus directory with given name at the current path"""
-    if directoryExists(parent, name):
-        db.dirs.remove({'parent': parent, 'name': name})
-    else:
+    if not directory_exists(parent, name):
         print "Error: '" + name + "' is not a valid directory."
+    elif not directory_empty(parent, name):
+        print "Error: '" + name + "' is not empty."
+    else:
+        db.dirs.remove({'parent': parent, 'name': name})
 
-def listFiles(path):
+def list_files(path):
     dirs = db.dirs.find({'parent': path}, {'name': 1, '_id': 0})
     for directory in dirs:
         print directory['name'] + "/"
--- lib/navigate.py
+++ lib/navigate.py
@@ -4,23 +4,22 @@
 """
 
 import os
-from lib import db
+import db
 
 COMMANDS = {'pwd', 'spwd', 'ls', 'sls', 'cd', 'scd', 'smkdir', 'srmdir'}
-
-spath = '/'
+spath = ''
 
 def print_help():
     """Prints the navigation help prompts"""
     print "\n# Navigation:"
-    print "  pwd               - get path of local directory"
-    print "  spwd              - get path of stratus directory"
-    print "  ls                - list files in local directory"
-    print "  sls               - list files in stratus directory"
-    print "  cd     [dir_path] - navigate to local directory at dir_path"
-    print "  scd    [dir_path] - navigate to stratus directory at dir_path"
-    print "  smkdir [dir_name] - create stratus directory at current path"
-    print "  srmdir [dir_name] - remove stratus directory at current path"
+    print "  pwd           - get path of local directory"
+    print "  spwd          - get path of stratus directory"
+    print "  ls            - list files in local directory"
+    print "  sls           - list files in stratus directory"
+    print "  cd     [path] - navigate to local directory at path"
+    print "  scd    [path] - navigate to stratus directory at path"
+    print "  smkdir [name] - create stratus directory at current path"
+    print "  srmdir [name] - remove stratus directory at current path"
 
 def process_command(args):
     cmd = args[0]
@@ -49,7 +48,7 @@ def pwd():
 
 def spwd():
     """Prints the absolute path of the current stratus directory"""
-    print spath
+    print '/' + spath
 
 def ls():
     """Lists all files and folders in the current local directory"""
@@ -61,23 +60,61 @@ def ls():
 
 def sls():
     """Lists all files and folders in the current stratus directory"""
-    db.listFiles(spath)
+    db.list_files(spath)
 
-def cd(dir_path):
-    """Move into the given local directory"""
-    path = os.path.expanduser(dir_path)
+def cd(path):
+    """Moves into the given local directory"""
+    path = os.path.expanduser(path)
     if os.path.isdir(path):
         os.chdir(path)
     else:
-        print "Error: '" + cd + "' is not a valid directory."
+        print "Error: '" + path + "' is not a valid directory."
 
-def scd(dir_name):
-    """Move into the given stratus directory from the current path"""
+def scd(path):
+    """Moves into the given stratus directory from the current path"""
+    abs_path = get_abs_path(path)
+    parent, name = split_path(abs_path)
+    if parent is None or db.directory_exists(parent, name):
+        global spath
+        spath = abs_path
+    else:
+        print "Error: '" + abs_path + "' is not a valid directory."
 
-def smkdir(dir_name):
+def smkdir(path):
     """Create a stratus directory from the current path"""
-    db.createDirectory(spath, dir_name)
+    abs_path = get_abs_path(path)
+    parent, name = split_path(abs_path)
+    db.create_directory(parent, name)
 
-def srmdir(dir_name):
+def srmdir(path):
     """Delete a stratus directory at the current path"""
-    db.removeDirectory(spath, dir_name)
+    abs_path = get_abs_path(path)
+    parent, name = split_path(abs_path)
+    db.remove_directory(parent, name)
+
+def get_abs_path(path):
+    """Resolves path into a clean, usable absolute stratus path"""
+    abs_path = spath
+    if path.startswith('/') or abs_path == '':
+        abs_path = path
+    else:
+        if path.startswith('./'):
+            path = path[2:]
+        while path.startswith('..'):
+            path = path.lstrip('..')
+            path = path.lstrip('/')
+            if abs_path.count('/') == 0:
+                abs_path = ''
+            else:
+                abs_path = abs_path.rsplit('/', 1)[0]
+        abs_path = abs_path + '/' + path
+    abs_path = abs_path.strip('/')
+    return abs_path
+
+def split_path(abs_path):
+    """Gets the parent and name of the directory at the given path"""
+    if abs_path == '': # root folder
+        return [None, '']
+    elif abs_path.count('/') == 0: # inside root
+        return ['', abs_path]
+    return abs_path.rsplit('/', 1)
